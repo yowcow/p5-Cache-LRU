704018b289856bb2af3bd7a4ccd6a6980aca6c79
murphy-warugaki <shinya.nishimoto@d8aspring.com>
2017-01-19 18:40:48 +0900

    second commit

--- lib/Cache/LRU.pm
+++ lib/Cache/LRU.pm
@@ -3,9 +3,21 @@ package Cache::LRU;
 use warnings;
 use utf8;
 use strict;
+use Data::Dumper;
 
 sub new {
-    my %self;
+    my $hash_ref = {};
+    my $class    = shift;
+
+    $hash_ref->{max_size} = 3;
+
+    return bless $hash_ref, $class;
+}
+
+sub max_size {
+    my $self = shift;
+
+    return $self->{max_size};
 }
 
 1;
